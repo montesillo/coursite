538c56afc327eb17293871aaae7dc36256c6c327
JESUS VALENCIA <jerv_ing94@outlook.com>
2018-08-03 16:03:39 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 $_SESSION['email']= $_POST['email'];
 $_SESSION['password']= $_POST['pass'];
-$consulta = "select * from alumnos where email= '$_POST[email]'";
+$consulta = "select * from alumnos where correo= '$_POST[email]'";
 include('consulta.php');
 	$base = new Acceso($consulta);
 	$base->UsuariosSQL();
--- registro.php
+++ registro.php
@@ -5,8 +5,6 @@ if (isset($_POST['enviar'])) {
 		$consulta = "insert into alumnos(nombres, a_paterno, a_materno, correo, password) values('$_POST[nombre]', '$_POST[paterno]', '$_POST[materno]', '$_POST[email]', '$_POST[password]')";
 		$base = new Acceso($consulta);
 		$base->InsertarSQL();
-	
-	
 }
 
  ?>
