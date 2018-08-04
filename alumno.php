a0aed6f298b042d7e5a3e9464f00d78fe8729a79
JESUS VALENCIA <jerv_ing94@outlook.com>
2018-08-03 23:38:19 -0500

    Add files via upload

---
+++ alumno.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$_SESSION['email']= $_POST['email'];
+$_SESSION['password']= $_POST['pass'];
+$consulta = "select * from alumnos where correo= '$_POST[email]'";
+include('consultaAlumnos.php');
+	$base = new Maestro($consulta);
+	$base->UsuariosSQL();
+
+?>
