3f14a93184d783c84a50e6474040362949302614
Hiram <31787646+montesillo@users.noreply.github.com>
2018-08-04 00:52:12 -0500

    Add files via upload

--- CrearCurso.php
+++ CrearCurso.php
@@ -6,62 +6,26 @@
 	<link rel="stylesheet" type="text/css" href="estilos/estilo.css">
 
 </head>
-<body>
+<body background="Imagenes/fondo.png">
 	<header>
-			<div class="ancho">
-				<nav>
-				<ul>
-					<li><a href="index.html">Inicio</a></li>
-					
-					<li><a href="index.html">Cerrar Sesión</a></li>
-
-				</ul>
-			</nav>
+			<div id="header">
+			<ul class="nav">
+				<li><a href="registro.html">Registro</a></li>
+				<li><a href="index.html">Inicio</a></li>
+			</ul>
 		</div>
-
-		
 	</header>
-	<div class="formulario">
-		<form action=".php" method="post">
-			<p>Registrate</p>
-			<p>Nombre: </p>
-			<input type="text" name="nombre" maxlength="35" id="nombre" >
-			<p>Apellido Paterno:</p>
-			<input type="text" name="paterno" maxlength="35" id="paterno">
-			<p>Apellido Materno:</p>
-			<input type="text" name="materno" maxlength="35" id="materno">
-			<p>Correo: </p>
-			<input type="mail" name="email" maxlength="35" id="email">
-			<p>Contraseña: </p>
-			<input type="password" name="password" maxlength="35" id="password">
-			<p>Repita su contraseña: </p>
-			<input type="password" name="password2" maxlength="35" id="password2"><br><br>
-			<input type="submit" name="enviar" onMouseOver="pass();">
+	<?php 
+		if (!empty($_COOKIE['maestro'])) {
+		header('location: maestro.html');
+	}
+	 ?>
 
-			<script type="text/javascript">
-				function pass(){
-					var name= document.getElementById('nombre').value;
-					var paterno= document.getElementById('paterno').value;
-					var materno= document.getElementById('materno').value;
-					var email= document.getElementById('email').value;			
-					var password = document.getElementById('password').value;
-					var password2 = document.getElementById('password2').value;
-					if (nombre =="" || paterno =="" || materno =="" || email =="" || password =="" || password2 =="") {
-						alert('Uno o más campos estan vacios');
-
-					}else if (password != password2) {
-						alert('Contraseñas diferentes');
-					}
-					
-				}
-
-			</script>
-
-		</form>		
-
+	<div class="botones">
+		<li><a  style="color: white" href="NuevoCurso.php">Nuevo Curso</a></li><br>
+		<li><a  style="color: white" href="CursosExistentes.php">Curso existente</a></li>
+	
 	</div>
-
-
 
 </body>
 </html>
