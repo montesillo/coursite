9b40144c492cbc51be4ddbeea7b531fb07fc71d8
JESUS VALENCIA <jerv_ing94@outlook.com>
2018-08-05 12:51:38 -0500

    Delete activar.php

--- activar.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-	$consulta = "UPDATE alumnos SET intentos = 0 WHERE alumnos.correo = '$_SESSION[email]'";
-	include('consultaAlumnos.php');
-	$base = new Alumno($consulta);
-	$base->Activar();
-?>
