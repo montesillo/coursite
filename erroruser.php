329d0ed2c95238c878836db1e827ed704bf4110d
JESUS VALENCIA <jerv_ing94@outlook.com>
2018-08-05 12:52:24 -0500

    Delete erroruser.php

--- erroruser.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-	$consulta = "UPDATE alumnos SET intentos = '$_GET[intentos]' WHERE alumnos.correo = '$_SESSION[email]'";
-	include('consultaAlumnos.php');
-
-	$base = new Alumno($consulta);
-	$base->ErrorPassSQL();
-?>
